ce2d227c504459e2b5abad6a037e20d1ca5673b9
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-10-22 21:09:21 +0900

    9.palindrome number
    https://leetcode.com/problems/palindrome-number/

--- leetcode_problem/leetcode-9-palindrome_number.py
+++ leetcode_problem/leetcode-9-palindrome_number.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-def solve(x):
-    if list(str(x)) == list(reversed(list(str(x)))):
-        return True
-    else: return False
-    
-print(solve(121))
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if list(str(x)) == list(reversed(list(str(x)))):
+            return True
+        else:
+            return False
