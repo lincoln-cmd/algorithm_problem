3702ae50b1ff070086bdc2401cc8c982ba0bd3d4
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2022-07-21 23:19:42 +0900

    703A - Mishka and Game
    703A - Mishka and Game

---
+++ codeforces/Mishka and Game.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+m, c = 0, 0
+
+for i in range(n):
+    a, b = map(int, input().split())
+    if a > b:
+        m += 1
+    elif b > a:
+        c += 1
+        
+if m > c:
+    print('Mishka')
+elif c > m:
+    print('Chris')
+else:
+    print('Friendship is magic!^^')
