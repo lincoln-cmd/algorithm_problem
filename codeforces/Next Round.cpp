7777d780ad71e3c261fa4d4f2f47c70218eaee43
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2022-08-27 15:15:08 +0900

    158A - Next Round
    158A - Next Round

---
+++ codeforces/Next Round.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(){
+    int n, k, cnt = 0;
+    cin >> n >> k;
+    int li[n];
+    for(int i = 0; i < n; i++){
+        cin >> li[i];
+    }
+    
+    for(int i = 0; i < n; i++){
+        if(li[i] >= li[k - 1] && li[i] > 0){
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+    
+    return 0;
+}
