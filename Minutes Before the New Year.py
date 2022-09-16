89d805d700da1dfc967f28af605410bed0888c86
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2022-09-16 23:09:34 +0900

    Delete Minutes Before the New Year.py

--- Minutes Before the New Year.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-
-for test in range(t):
-    h, m = map(int, input().split())
-    
-    if m == 0:
-        print((24 - h) * 60)
-    else:
-        print((23 - h) * 60 + (60 - m))
